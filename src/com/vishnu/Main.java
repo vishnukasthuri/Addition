39d9458fa0750892db62495da3df7131cb396814
vishnukasthuri <vishnuvk1693@gmail.com>
2022-01-18 22:20:19 +0530

    Addition

---
+++ src/com/vishnu/Main.java
@@ -0,0 +1,11 @@
+package com.vishnu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i,j;
+        i = 10;
+        j = 20;
+        System.out.println(i+j);
+    }
+}
